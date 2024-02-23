9970003d5f382e47ebc8f450eea6f31752b5c894
regi1297 <reginald_miranda@hotmail.com>
2024-02-23 20:48:29 -0300

    Created avangers api firts version

---
+++ src/test/kotlin/one/digitalinnovationavengers/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovationavengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
